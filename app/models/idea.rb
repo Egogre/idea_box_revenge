5f645a83aafebfb083a2984a92f75f8f067de34f
George Hudson <georgehudson78@gmail.com>
2015-11-30 15:28:04 -0700

    built in upgrade and downgrade of quality. added validations

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,20 @@
 class Idea < ActiveRecord::Base
-
+  validates :title, :body, presence: true
   enum quality: [:swill, :plausible, :genius]
+
+  def upgrade
+    if self.swill?
+      self.quality = 1
+    else
+      self.quality = 2
+    end
+  end
+
+  def downgrade
+    if self.genius?
+      self.quality = 1
+    else
+      self.quality = 0
+    end
+  end
 end
--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -19,5 +19,58 @@ class IdeaTest < ActiveSupport::TestCase
     assert_equal "swill", swill_idea.quality
   end
 
+  test "idea is invalid without title or body" do
+    no_title = Idea.new(body: "World")
+    no_body = Idea.new(title: "Hello")
+
+    refute no_title.valid?
+    refute no_body.valid?
+  end
+
+  test "idea quality can be upgraded" do
+    better_idea = Idea.create(title: "Better Idea", body: "Justin Beaber punching bag!")
+    better_idea.upgrade
+
+    assert_equal "plausible", better_idea.quality
+
+    better_idea.upgrade
+
+    assert_equal "genius", better_idea.quality
+  end
+
+  test "idea quality can't be higher than genius" do
+    super_genius_idea = Idea.create(title: "Better Idea", body: "Justin Beaber proof hearing protection!")
+    super_genius_idea.upgrade
+    super_genius_idea.upgrade
+
+    assert_equal "genius", super_genius_idea.quality
+
+    super_genius_idea.upgrade
+    assert_equal "genius", super_genius_idea.quality
+  end
+
+  test "idea quality can be downgraded" do
+    better_idea = Idea.create(title: "Better Idea", body: "Justin Beaber punching bag!")
+    better_idea.upgrade
+    better_idea.upgrade
+
+    assert_equal "genius", better_idea.quality
+
+    better_idea.downgrade
+
+    assert_equal "plausible", better_idea.quality
+
+    better_idea.downgrade
+
+    assert_equal "swill", better_idea.quality
+  end
+
+  test "idea quality can't be lower than swill" do
+    ultra_swill_idea = Idea.create(title: "Worse Than Swill Idea", body: "Justin Beaber amplifying headphones!")
+    ultra_swill_idea.downgrade
+
+    assert_equal "swill", ultra_swill_idea.quality
+  end
+
 
 end
