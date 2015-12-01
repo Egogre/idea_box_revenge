d096489335823cc7365b94bbe4800cddccd9bfd3
George Hudson <georgehudson78@gmail.com>
2015-11-30 20:09:26 -0700

    built static display root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
       resources :ideas, only: [:index, :create, :update, :destroy], defaults: { format: :json }
     end
   end
+
+  root "ideas#index"
 end
