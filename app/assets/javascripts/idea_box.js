032e64eff763ce6a6b7d2bf02ab226a8ad9a3380
George Hudson <georgehudson78@gmail.com>
2015-12-02 23:44:34 -0700

    search on keyup working. using poltergeist for Capybara js driver

--- app/assets/javascripts/idea_box.js
+++ app/assets/javascripts/idea_box.js
@@ -4,18 +4,33 @@ $(document).ready(function(){
   $('#idea-save').on('click', function(){
     createIdea($ideas, this);
   });
+
+  $('#search').keyup(function() {
+    var $cards = $ideas.children();
+    var searchValue = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+    $cards.show().filter(function() {
+        var cardText = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~cardText.indexOf(searchValue);
+    }).hide();
+  });
+
   $ideas.on('click', '.delete-button', function(){
     deleteIdea(this);
   });
+
   $ideas.on('click', '.edit-idea', function(){
     editWindow(this);
   });
+
   $ideas.on('click', '.edit-update', function(){
     editIdea(this);
   });
+
   $ideas.on('click', '.downgrade', function(){
     downgradeIdea(this);
   });
+
   $ideas.on('click', '.upgrade', function(){
     upgradeIdea(this);
   });
